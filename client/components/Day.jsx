c5e87a03bce5d046dc047f8870dafd5b029fe83d
joanna-chen <joannachen.x@gmail.com>
2017-05-27 16:16:45 -0400

    some lines

--- client/components/Day.jsx
+++ client/components/Day.jsx
@@ -1,10 +1,209 @@
 import React from 'react';
+// import Hour;
 
 export default class Day extends React.Component {
+  // renderHour() {
+  //   return <Hour value={0}/>;
+  // }
+
   render() {
     return (
       <div className="day">
-        Day
+        <h1>SATURDAY MAY 27</h1>
+        <div className="hours">
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              1
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              2
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              3
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+          <div className="hour">
+            <div className="title">
+              12
+              AM
+            </div>
+            <div className="space">
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ client/components/Event.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Event extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className="event">
+
+      </div>
+    );
+  }
+}
