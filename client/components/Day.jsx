abe6d52e5663810aeb2b3104c80d741b9db46f1c
Andy Zhang <andzhng@gmail.com>
2017-05-27 14:44:45 -0400

    Added day styling

--- client/components/Day.jsx
+++ client/components/Day.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Day extends React.Component {
   render() {
     return (
-      <div>
+      <div className="day">
         Day
       </div>
     );
