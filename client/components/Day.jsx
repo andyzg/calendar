1fa6d9ef340e4f976f22f0ce2845c49e8a015262
Andy Zhang <andzhng@gmail.com>
2017-05-27 17:16:45 -0400

    Fixed day calendar stuff

--- client/components/Day.jsx
+++ client/components/Day.jsx
@@ -2,205 +2,63 @@ import React from 'react';
 // import Hour;
 
 export default class Day extends React.Component {
-  // renderHour() {
-  //   return <Hour value={0}/>;
-  // }
+  renderHour(hour) {
+    var key = hour;
+    var type = "AM";
+    if (hour >= 12) {
+      type = "PM";
+      hour -= 12;
+    }
+
+    return (
+      <div className="hour" key={key}>
+        <div className="title">
+          {hour === 0 ? 12 : hour} <br />
+          {type}
+        </div>
+        <div className="hour-line"></div>
+      </div>
+    );
+  }
+
+  renderEvent(hour, minutes, length, title) {
+    var top = hour * 80;
+    if (minutes >= 30) {
+      top += 40;
+    }
+    console.log(hour);
+    var bottom = (24 - hour) * 80;
+    if (minutes >= 30) {
+      bottom -= 40;
+    }
+    bottom -= (length / 30) * 40;
+
+    var style = {
+      top: top,
+      bottom: bottom
+    };
+
+    return (
+      <div style={style} className="event">
+        {title}
+      </div>
+    );
+  }
 
   render() {
+    var hours = [];
+    for (var i = 0; i < 24; i++) {
+      hours.push(this.renderHour(i));
+    }
+
     return (
       <div className="day">
-        <h1>SATURDAY MAY 27</h1>
+        <h1>SATURDAY, MAY 27</h1>
         <div className="hours">
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              1
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              2
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              3
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
-            </div>
-          </div>
-          <div className="hour">
-            <div className="title">
-              12
-              AM
-            </div>
-            <div className="space">
+          {hours}
+          <div className="events">
+            <div className="events__container">
+              {this.renderEvent(13, 30, 30, "Hello world")}
             </div>
           </div>
         </div>
