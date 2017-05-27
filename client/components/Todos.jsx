8a9d568d2788d307c918988ade57900f4200a4ad
Andy Zhang <andzhng@gmail.com>
2017-05-27 15:49:02 -0400

    Todos section

--- client/components/Todos.jsx
+++ client/components/Todos.jsx
@@ -1,10 +1,73 @@
 import React from 'react';
 
 export default class Todos extends React.Component {
+
+  getInitialState() {
+    return {
+      tasks: []
+    }
+  }
+
   render() {
     return (
-      <div>
-        Todos
+      <div className="todos">
+        <div className="todos__day-row">
+          <h2 className="todos__day">Saturday, May 27 </h2>
+          <div className="todos__day-switcher">
+            <img src="./img/left.png" />
+          </div>
+          <div className="todos__day-switcher">
+            <img src="./img/right.png" />
+          </div>
+        </div>
+        <div className="todos__section">
+          <span className="todos__section__header late">Late</span>
+          <div className="todos__list">
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+          </div>
+        </div>
+        <div className="todos__section">
+          <span className="todos__section__header">Inbox</span>
+          <span className="bubble"></span>
+          <div className="todos__list">
+            <TodoInput />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class TodoItem extends React.Component {
+  render() {
+    return (
+      <div className="todos__item">
+        <div className="todos__item__checkbox">
+          <img src="img/checkbox.png" />
+        </div>
+        <div className="todos__item__title">
+          Todo item
+        </div>
+      </div>
+    );
+  }
+}
+
+class TodoInput extends React.Component {
+  render() {
+    return (
+      <div className="todos__input">
+        <div className="todos__input__add">
+          <img src="img/add.png" />
+        </div>
+        <input className="todos__input__text" type="text" placeholder="Add a new task" />
       </div>
     );
   }
